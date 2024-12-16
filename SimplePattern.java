62547f6a70fbb8a4fbe7a652f4d58329e063c6bf
AjayGangadhara <ajaygangadhara66@gmail.com>
2024-12-16 21:12:56 +0530

    Update SimplePattern.java

--- SimplePattern.java
+++ SimplePattern.java
@@ -1,1 +1,18 @@
+public class SimplePattern {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows for the pattern
 
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
